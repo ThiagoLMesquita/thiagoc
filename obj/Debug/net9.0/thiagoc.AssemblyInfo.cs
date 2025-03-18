1232ccb03bde4fb7eb3dada51ff6d46779258c4d
John Doe <thiagolima90br@gmail.com>
2025-03-18 20:02:18 -0300

    renamed:    Program.cs -> Notas.cs 	new file:   Senha.cs 	new file:   obj/Debug/net9.0/thiagoc.sourcelink.json

--- obj/Debug/net9.0/thiagoc.AssemblyInfo.cs
+++ obj/Debug/net9.0/thiagoc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("thiagoc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f879b86ca5b45fc1df90463e34edd5148ab89087")]
 [assembly: System.Reflection.AssemblyProductAttribute("thiagoc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("thiagoc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
